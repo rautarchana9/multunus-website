d9acddb38e74fd7c849ef3b64224fab3bc6ba05d
Sreenath N <tacticiankerala@gmail.com>
2016-04-20 16:54:37 +0530

    Add styles for team section in about us page and team widget
    - Also, close description shown when clicked on the team member

--- source/javascripts/team_wdiget.js
+++ source/javascripts/team_wdiget.js
@@ -17,15 +17,21 @@ $(function (){
         $('.team-widget .member-description').remove();
         var elem = $(e.target);
         var memberElem =  elem.hasClass('member') ? elem : elem.parent('.member');
-        var memberDescriptionElem = memberElem.find('.description');
-        if(memberDescriptionElem.length > 0) {
-            var wrappingContainerElem = memberElem.parent('.row');
-            var lastElementInSameRow = findLastMemberInTheSameRow(memberElem);
-            var caretMarkup = "<div class='triangle-up'><div class='inner-triangle'></div></div>";
-            lastElementInSameRow.after("<div class='col-xs-12 member-description'>" + caretMarkup + memberDescriptionElem.html() +"</div>");
-            var caretElem = $('.member-description .triangle-up');
-            var caretLeftOffset = (memberElem.offset().left - wrappingContainerElem.offset().left) + (memberElem.outerWidth() / 2.0) - (caretElem.outerWidth() / 2.0);
-            $('.member-description .triangle-up').css('left', caretLeftOffset);
+        if(!memberElem.hasClass('active')) {
+            var memberDescriptionElem = memberElem.find('.description');
+            if(memberDescriptionElem.length > 0) {
+                $('.team-widget .member.active').removeClass('active');
+                memberElem.addClass('active');
+                var wrappingContainerElem = memberElem.parent('.row');
+                var lastElementInSameRow = findLastMemberInTheSameRow(memberElem);
+                var caretMarkup = "<div class='triangle-up'><div class='inner-triangle'></div></div>";
+                lastElementInSameRow.after("<div class='col-xs-12 member-description'>" + caretMarkup + memberDescriptionElem.html() +"</div>");
+                var caretElem = $('.member-description .triangle-up');
+                var caretLeftOffset = (memberElem.offset().left - wrappingContainerElem.offset().left) + (memberElem.outerWidth() / 2.0) - (caretElem.outerWidth() / 2.0);
+                $('.member-description .triangle-up').css('left', caretLeftOffset);
+            }
+        } else {
+            memberElem.removeClass('active');
         }
     });
 });
