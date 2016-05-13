c98079e6203c08ee904bd80509227a8cd1256a29
Archana Raut <rautarchana9@gmail.com>
2016-05-13 16:47:17 +0530

    refactor the contact form postion function

--- source/javascripts/google-sheet.js
+++ source/javascripts/google-sheet.js
@@ -8,43 +8,22 @@ $(function (){
     if (request) {
       request.abort();
     }
-    var name = $('#InputName').val();
-    var email = $('#InputEmail').val();
-    var message = $('#InputHelp').val();
-    var payload = {"name": name, "email": email, "message": message}
+    var formInputs  = getFormInputs();
 
-    if( name == '' || email == '' || message == '' || !(validateEmail(email))) {
+    if( validateFormInputs(formInputs)) {
       return false;
     } else {
       this.innerHTML='Please wait ...'
-      $(this).prop("disabled",true);
-      // Here we fire the request to google sheet
-      request = $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbwFIuDTVouqc6sLiEQs0uCH63zokYWAm6kp9DuGuqaLzAU2Eo8/exec",
-        type: "post",
-        data: payload
-      });
-      var button = this
+    $(this).prop("disabled",true);
+  // Here we fire the request to google sheet
+  request = sendRequest(formInputs);
+  var button = this
 
-      // Callback handler that will be called on success
-      request.done(function (response, textStatus, jqXHR){
-        // Log a message to the console
-        console.log(textStatus);
-        button.innerHTML='Submitted'
-        $('.form-group').hide();
-        $('.feedback').show();
-      });
-
-      // Callback handler that will be called on failure
-      request.fail(function (jqXHR, textStatus, errorThrown){
-        // Log the error to the console
-        console.error(
-          "The following error occurred: "+
-            textStatus, errorThrown
-        );
-      });
-
-      event.preventDefault();
+    // Callback handler that will be called on success
+    onSuccess(request, button);
+  // Callback handler that will be called on failure
+  onFailure(request);
+  event.preventDefault();
     }
   });
 });
@@ -53,4 +32,42 @@ function validateEmail(email)
 {
   var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return re.test(email);
+}
+
+function validateFormInputs(formInputs){
+  return ( formInputs['name'] == '' || formInputs['email'] == '' || formInputs['message'] == '' || !(validateEmail(formInputs['email'])));
+}
+
+function getFormInputs(){
+  var name = $('#InputName').val();
+  var email = $('#InputEmail').val();
+  var message = $('#InputHelp').val();
+  return {'name': name, 'email': email, 'message': message};
+}
+
+function sendRequest(formInputs){
+  return $.ajax({
+    url: "https://script.google.com/macros/s/AKfycbwFIuDTVouqc6sLiEQs0uCH63zokYWAm6kp9DuGuqaLzAU2Eo8/exec",
+         type: "post",
+         data: formInputs});
+
+}
+function onSuccess(request, button){
+  request.done(function (response, textStatus, jqXHR){
+    // Log a message to the console
+    console.log(textStatus);
+    button.innerHTML='Submitted'
+    $('.form-group').hide();
+  $('.feedback').show();
+  });  
+}
+
+function onFailure(request){
+  request.fail(function (jqXHR, textStatus, errorThrown){
+    // Log the error to the console
+    console.error(
+      "The following error occurred: "+
+      textStatus, errorThrown
+      );
+  });
 }
